7b61e7000315325170ff2749153e30f4ed686521
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-04-18 21:24:29 -0500

    Add pycuda hello world

---
+++ pycuda/hello_world.py
@@ -0,0 +1,17 @@
+
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+mod = SourceModule("""
+    #include <stdio.h>
+
+    __global__ void kernel()
+    {
+        printf("Hello, World!\\n");
+    }""")
+
+func = mod.get_function("kernel")
+
+
+func(block=(1, 1, 1))
